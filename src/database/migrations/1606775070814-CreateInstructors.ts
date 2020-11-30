e61a280da53cdc67b31d8fa12b50e16abe07a9d9
Luke0195 <lucas201273@gmail.com>
2020-11-30 20:27:42 -0300

    feat: add instructor migrate

---
+++ src/database/migrations/1606775070814-CreateInstructors.ts
@@ -0,0 +1,74 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateInstructors1606775070814
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await queryRunner.createTable(
+      new Table({
+        name: 'instructors',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isNullable: false,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+          },
+
+          {
+            name: 'gender',
+            type: 'varchar',
+            isNullable: false,
+          },
+
+          {
+            name: 'birth',
+            type: 'date',
+            isNullable: false,
+          },
+
+          {
+            name: 'services',
+            type: 'varchar',
+            isNullable: false,
+          },
+
+          {
+            name: 'avatar_url',
+            type: 'varchar',
+            isNullable: false,
+          },
+
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('instructors');
+  }
+}
