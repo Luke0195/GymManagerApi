159c6d6e31ee9b59dfa443736218395da939c05c
Luke0195 <lucas201273@gmail.com>
2020-11-30 18:21:21 -0300

    chore: done configuration of eslint, prettier,editor-config

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'Welcome to the gymManger' }));
+routes.get('/', (request, response) =>
+  response.json({ message: 'Welcome to the gymManger' })
+);
 
 export default routes;
