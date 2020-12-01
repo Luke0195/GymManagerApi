8e619b02a26fe58418428f65eec096ad02e59805
Luke0195 <lucas201273@gmail.com>
2020-12-01 09:31:54 -0300

    feat: add list instructor routes

--- src/routes/instructorsRoutes.ts
+++ src/routes/instructorsRoutes.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
+import Instructor from '../models/Instructor';
 import CreateInstructorService from '../services/CreateInstructorService';
 
 const instructorsRoutes = Router();
 
-instructorsRoutes.get('/', (request, response) => {
-  return response.json({ message: 'Route of instructors' });
+instructorsRoutes.get('/', async (request, response) => {
+  try {
+    const instructorRepository = getRepository(Instructor);
+    const instructors = await instructorRepository.find();
+    response.json(instructors);
+  } catch (error) {
+    response.json(error.message);
+  }
 });
 
 instructorsRoutes.post('/', async (request, response) => {
